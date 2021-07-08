08c4c62bf2cbea55eb05f83bb9c73fcbd8702236
Cooper Saye <coopersaye@gmail.com>
2021-07-08 15:29:48 -0700

    Create footers.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -6,6 +9,7 @@ import 'firebase/firestore';
 import { firebaseConfig } from '../firebaseConfig.js';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
+import styles from '../styles/App.module.css';
 import '../styles/globals.css';
 
 // initialize firebase
@@ -21,6 +25,7 @@ function App({ Component, pageProps }) {
       <Head>
         <title>MovieTalk</title>
         <meta name="description" content="A social media based around movies." />
+        {/* icons */}
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
@@ -28,6 +33,42 @@ function App({ Component, pageProps }) {
       </Head>
       <div className={styles.background} />
       <Component {...pageProps} />
+      <p className={styles.lfooter}>
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterIcon />
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+      </p>
+      <p className={styles.rfooter}>
+        © MovieTalk {new Date().getFullYear()}.
+        Film data from{' '}
+        <a
+          className="styled"
+          href="https://www.themoviedb.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TMDb
+        </a>
+        .
+      </p>
     </>
   );
 }
