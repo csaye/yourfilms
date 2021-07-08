fdb564c04c4a9cf1fcbcd879fdea51196c139bac
Cooper Saye <coopersaye@gmail.com>
2021-07-07 23:34:57 -0700

    Display user data.

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -6,6 +6,21 @@ import firebase from 'firebase/app';
 export default function UserPage(props) {
   return (
     <div>
+      <Link href="/">
+        <a>Back</a>
+      </Link>
+      {
+        props.data ?
+        <div>
+          {
+            props.data.id === firebase.auth().currentUser?.uid &&
+            <i>your page</i>
+          }
+          <h1>{props.data.name}</h1>
+          <p>Joined {new Date(props.data.joined).toLocaleString()}</p>
+        </div> :
+        <p>User not found</p>
+      }
     </div>
   );
 }
@@ -21,7 +36,7 @@ export async function getStaticProps({ params }) {
   // retrieve public user data from id
   const userDoc = await firebase.firestore()
   .collection('users-public').doc(params.id).get();
-  const data = userDoc.exists ? userDoc.data() : null;
+  const data = userDoc.exists ? { ...userDoc.data(), id: userDoc.id } : null;
   // return data revalidating every minute
   return {
     props: {
