860e4be03ad4e97966a45521548ad61fdb8207f7
Cooper Saye <coopersaye@gmail.com>
2021-07-07 22:46:47 -0700

    Implement signin function.

--- pages/signin.js
+++ pages/signin.js
@@ -10,6 +10,27 @@ export default function SignIn() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  async function signIn() {
+    setError('');
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+    } catch(e) {
+      if (e.code === 'auth/invalid-email') {
+        setError('Invalid email address.');
+      } else if (e.code === 'auth/user-not-found') {
+        setError('Unknown email address.');
+      } else if (e.code === 'auth/wrong-password') {
+        setError('Incorrect password. Please try again.');
+      } else if (e.code === 'auth/too-many-requests') {
+        setError('Too many sign in requests. Please try again later.')
+      } else if (e.code === 'auth/weak-password') {
+        setError('Password must be at least 6 characters.')
+      } else {
+        setError(e.message);
+      }
+    }
+  }
+
   return (
     <div>
       <h1>Sign In</h1>
