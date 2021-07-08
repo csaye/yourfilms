cc4cd787396b04dbcd6d675835344a75b17e0a35
Cooper Saye <coopersaye@gmail.com>
2021-07-07 22:40:55 -0700

    Import favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,10 @@ function App({ Component, pageProps }) {
       <Head>
         <title>MovieTalk</title>
         <meta name="description" content="A social media based around movies." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </>
