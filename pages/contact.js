3de67df958bd61ada070a8631d20291591e0bc7b
Cooper Saye <coopersaye@gmail.com>
2021-07-27 23:39:29 -0700

    Improve contact page styling.

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,6 @@
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 
@@ -9,6 +12,29 @@ export default function Contact() {
       <Header />
       <div className={styles.content}>
         <h1>Contact</h1>
+        <div className={styles.iconlist}>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TwitterIcon />
+          </a>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FacebookIcon />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramIcon />
+          </a>
+        </div>
       </div>
       <Footer />
     </>
