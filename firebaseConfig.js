8761fab6beb542ef1182771ac1c6deda08d3b4be
Cooper Saye <coopersaye@gmail.com>
2021-07-27 17:52:35 -0700

    Prepare for deploy.

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAM2TD3l0_44W3XuoyMWIMezI9IYy6JttA",
+  authDomain: "movie-talk-app.firebaseapp.com",
+  projectId: "movie-talk-app",
+  storageBucket: "movie-talk-app.appspot.com",
+  messagingSenderId: "84525042340",
+  appId: "1:84525042340:web:bd927aab5486bb1810b431",
+  measurementId: "G-VPQ4GBD0V8"
+};
