e5e9e7eda08c9392400a87af31b0584a5677ec07
Cooper Saye <coopersaye@gmail.com>
2021-07-28 12:38:19 -0700

    Implement genre filtering.

--- pages/movies.js
+++ pages/movies.js
@@ -4,11 +4,19 @@ import Footer from '../components/Footer.js';
 
 import { useState } from 'react';
 import firebase from 'firebase/app';
+import { genres } from '../data/genres.js';
 
 import styles from '../styles/Movies.module.css';
 
 export default function Movies(props) {
   const [sortBy, setSortBy] = useState('popularity');
+  const [chosenGenres, setChosenGenres] = useState([]);
+
+  // filter movies by chosen genres
+  const movies = chosenGenres.length ?
+  props.movies.filter(movie =>
+    movie.genre_ids.some(id => chosenGenres.includes(id))
+  ) : props.movies;
 
   return (
     <>
@@ -23,28 +31,48 @@ export default function Movies(props) {
             <option value="vote_average">Vote Average</option>
             <option value="vote_count">Vote Count</option>
           </select>
+          {
+            genres.map(genre =>
+              <div key={genre.id}>
+                <label htmlFor={`genre-${genre.id}`}>{genre.name}</label>
+                <input
+                  id={`genre-${genre.id}`}
+                  type="checkbox"
+                  checked={chosenGenres.includes(genre.id)}
+                  onChange={e => {
+                    // update chosen genres
+                    const genreIndex = chosenGenres.indexOf(genre.id);
+                    const newGenres = chosenGenres.slice();
+                    if (genreIndex === -1) newGenres.push(genre.id);
+                    else newGenres.splice(genreIndex, 1);
+                    setChosenGenres(newGenres);
+                  }}
+                />
+              </div>
+            )
+          }
         </div>
         <div className={styles.movielist}>
           {
             sortBy === 'popularity' &&
-            props.movies.sort((a, b) => b.popularity - a.popularity)
+            movies.sort((a, b) => b.popularity - a.popularity)
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
           {
             sortBy === 'release_date' &&
-            props.movies.sort((a, b) =>
+            movies.sort((a, b) =>
               new Date(b.release_date) - new Date(a.release_date)
             )
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
           {
             sortBy === 'vote_average' &&
-            props.movies.sort((a, b) => b.vote_average - a.vote_average)
+            movies.sort((a, b) => b.vote_average - a.vote_average)
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
           {
             sortBy === 'vote_count' &&
-            props.movies.sort((a, b) => b.vote_count - a.vote_count)
+            movies.sort((a, b) => b.vote_count - a.vote_count)
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
         </div>
