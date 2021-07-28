d7569497e73099550484d51872911a3460bfea93
Cooper Saye <coopersaye@gmail.com>
2021-07-27 23:05:36 -0700

    Improve page layout.

--- pages/recs.js
+++ pages/recs.js
@@ -5,12 +5,12 @@ import styles from '../styles/Recs.module.css';
 
 export default function Recs() {
   return (
-    <div className={styles.container}>
+    <>
       <Header />
       <div className={styles.content}>
         <h1>Your Recommendations</h1>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
