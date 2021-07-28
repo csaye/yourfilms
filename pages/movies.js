39a8f468a6cee547c053909cdb25eede0007f179
Cooper Saye <coopersaye@gmail.com>
2021-07-28 11:50:17 -0700

    Retrieve popular movies automatically.

--- pages/movies.js
+++ pages/movies.js
@@ -4,11 +4,10 @@ import Footer from '../components/Footer.js';
 
 import { useState } from 'react';
 import firebase from 'firebase/app';
-import { movies } from '../data/movies.js';
 
 import styles from '../styles/Movies.module.css';
 
-export default function Movies() {
+export default function Movies(props) {
   const [sortBy, setSortBy] = useState('popularity');
 
   return (
@@ -28,24 +27,24 @@ export default function Movies() {
         <div className={styles.movielist}>
           {
             sortBy === 'popularity' &&
-            movies.sort((a, b) => b.popularity - a.popularity)
+            props.movies.sort((a, b) => b.popularity - a.popularity)
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
           {
             sortBy === 'release_date' &&
-            movies.sort((a, b) =>
+            props.movies.sort((a, b) =>
               new Date(b.release_date) - new Date(a.release_date)
             )
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
           {
             sortBy === 'vote_average' &&
-            movies.sort((a, b) => b.vote_average - a.vote_average)
+            props.movies.sort((a, b) => b.vote_average - a.vote_average)
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
           {
             sortBy === 'vote_count' &&
-            movies.sort((a, b) => b.vote_count - a.vote_count)
+            props.movies.sort((a, b) => b.vote_count - a.vote_count)
             .map(movie => <Movie key={movie.id} data={movie} />)
           }
         </div>
@@ -53,4 +52,24 @@ export default function Movies() {
       <Footer />
     </>
   );
+}
+
+export async function getStaticProps() {
+  // retrieve top 60 popular movies
+  let movies = [];
+  const baseUrl = `https://api.themoviedb.org/3/movie/popular`;
+  for (let page = 1; page < 4; page++) {
+    // retrieve movies for each page
+    const url = `${baseUrl}?page=${page}&api_key=${process.env.TMDB_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    movies = movies.concat(data.results);
+  }
+  // return data revalidating every day
+  return {
+    props: {
+      movies: movies
+    },
+    revalidate: 86400
+  };
 }
